25bd48eed32ca5e18412e9e6cb6efcbf08023f74
Leon Brocard <acme@astray.com>
2010-01-11 12:03:23 +0530

    perltidy

--- lib/Dackup/Target/Filesystem.pm
+++ lib/Dackup/Target/Filesystem.pm
@@ -33,6 +33,7 @@ sub entries {
     my @entries;
     until ( $file_stream->is_done ) {
         foreach my $filename ( $file_stream->items ) {
+
             # Do not backup the cache db
             next if $filename->basename() eq 'dackup.db';
 
